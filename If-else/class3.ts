93ec7c938fde3fdc07e46195cd751d8faba3f80e
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-02-25 01:10:33 +0500

    If-else

---
+++ If-else/class3.ts
@@ -0,0 +1,20 @@
+//if-else
+
+/*let gate = 'hassan'
+
+if(gate === 'shahid'){
+console.log('hello, you can come!')
+} 
+else if(gate === 'hammad'){console.log('please come in')}
+else {console.log('no,you cant come')}*/
+
+//ticketing system
+
+
+
+let country = "Dubai"
+let age =10
+if(country === "Pakistan"){ if (age >= 18){console.log('here is your ticket')}
+
+else {console.log('age restricion')}
+} else {console.log('Invalid country')}

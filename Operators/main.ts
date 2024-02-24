051c5c4ad9cc9df632c5eb4f968abb0d149caa97
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-02-25 01:08:54 +0500

    Operators

--- Operators/main.ts
+++ Operators/main.ts
@@ -1,1 +1,68 @@
+ //operators
+ //arthematic
+ //addtion
+
+
+ /*let n1: number =20
+ let n2: number =10
+ console.log(n1 + n2)*/
+
+ //subtraction
+
+ /*let n1: number = 30
+ let n2: number = 12
+ console.log(n1 - n2)*/
+
+ //multiplication
+
+ /*let n1: number = 8
+ let n2: number = 7
+ console.log(n1 * n2)*/
+
+//division
+
+/*let n1: number = 30
+let n2: number = 6
+console.log(n1 / n2)*/
+
+//exponential
+
+/*let n1:number = 9
+let n2:number = 3
+console.log(n1 ** n2)*/
+
+//assignment operator
+
+let a = 7
+//console.log(a === 7)//t
+//console.log(a != 7)//f
+//console.log(a > 7)//f
+//console.log(a < 7)//f
+//console.log(a >= 7)//t
+//console.log(a <= 7)//
+
+
+//logocal operators
+
+let l = 5
+//console.log(l >= 5 && l< 5)//f
+//console.log(l<=5 || l> 5)//t
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

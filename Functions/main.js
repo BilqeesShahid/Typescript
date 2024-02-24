9c58347b131f358928a97137807b389d4139456d
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-02-25 01:12:32 +0500

    Functions

---
+++ Functions/main.js
@@ -0,0 +1,23 @@
+"use strict";
+///function
+// parameter=>>> (homework:string)
+// argument =>>> student
+/* function student(homework:string){
+    console.log('Do your homework')
+}
+
+student('')*/
+/*function hi(variable:string){
+   console.log(variable)
+}
+hi('hello everyone')*/
+//arrow / lambda function
+/*let myfunc=(a:number , b:number)=>
+console.log(a , b)
+console.log(myfunc(8,9))*/
+//return
+/*function checking(a:number , b:number){
+
+    return console.log(a + b);
+}
+    checking(4,3)*/
--- Functions/main.ts
+++ Functions/main.ts
@@ -1,1 +1,46 @@
+ ///function
+ // parameter=>>> (homework:string)
+ // argument =>>> student
+
+/* function student(homework:string){
+    console.log('Do your homework')
+}
+
+student('')*/
+
+/*function hi(variable:string){
+   console.log(variable) 
+}
+hi('hello everyone')*/
+
+
+//arrow / lambda function
+
+/*let myfunc=(a:number , b:number)=>
+console.log(a , b)
+console.log(myfunc(8,9))*/
+//return
+
+
+/*function checking(a:number , b:number){
+
+    return console.log(a + b);
+}
+    checking(4,3)*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

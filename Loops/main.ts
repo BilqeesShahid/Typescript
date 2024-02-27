55e868d8e880d99dd247f31e7545183eaf6fd1f0
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-02-28 01:03:53 +0500

    Loops

--- Loops/main.ts
+++ Loops/main.ts
@@ -1,1 +1,44 @@
+//for loop
+//loop format/syntax
+
+/*for(let i=1; i<9; i++){
+    console.log('Hello World')
+}*/
+
+/*let cleanestCities = ['Lahore','Islamabad','Peshawar']
+
+for(var i=0; i<=5; i++){
+    if('islamabad' === cleanestCities [i]){
+    console.log("It is one of the cleanest City");
+    break;
+
+    }
+}*/
+
+
+/*let n: number = 6
+let a: string =''
+for(let i:number=1; i<=6; i++){
+     for(let j:number=1; j<=i; j++){
+        a += "*"
+     }
+        a += "\n"
+        
+     console.log(a)
+}
+console.log(a)*/
+     
+/*let number = 5
+
+for(let i=1; i<=10; i++){
+
+console.log(`${number} * ${i} = ${number * i}`)}*/
+
+
+
+
+
+
+
+
 

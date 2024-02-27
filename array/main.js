7bc98c86b16227e3aee19a40c78611312c473c5b
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-02-28 00:51:34 +0500

    array

---
+++ array/main.js
@@ -0,0 +1,24 @@
+"use strict";
+//array
+//adding and removing element
+let fruits = ["apple", "orange", "banana", "grapes", "strawberry"];
+//console.log(fruits)
+//console.log(fruits[3])
+//console.log(fruits.pop())
+//console.log(fruits)
+//console.log(fruits.push('kiwi'))
+//console.log(fruits)
+//array shift and unshift
+//console.log(fruits.shift())
+//console.log(fruits)
+//console.log(fruits.unshift())
+//console.log(fruits)
+//console.log(fruits.unshift('pineapple'))
+//console.log(fruits)
+//console.log(fruits.splice(1,3,'mango','berries'))
+//console.log(fruits)
+//console.log(fruits.slice(3))
+//console.log(fruits)
+//let newvar=fruits.slice(1)
+//console.log(newvar)
+//console.log(fruits)

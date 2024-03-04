f4bbab5afe5792eaa8d0779ca4ef4e796fb57583
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-03-05 03:32:57 +0500

    Q6-10

---
+++ main.ts
@@ -0,0 +1,55 @@
+//Assignment 6
+//Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+
+/*let personName:string = "\t\n  Haroon Shahid  \n\t"
+console.log("Original:", personName)
+console.log("Stripped:",personName.trim())*/
+
+//Assignment 7
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+/*console.log(5+3)
+console.log(10-2)
+console.log(2*4)
+console.log(16/2)*/
+
+//Assignment 8
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+/*let favoriteNumber:number = 7
+console.log(`My favorite number is ${favoriteNumber}`)*/
+
+//Assignment 9
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//Author : Bilqees Shahid
+//date : March 1st 2024
+
+//printing my favourite number in Assignment 8
+
+ //Assignment 10
+// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+/*let friends =["Sara","Yusra","Fatima"]
+console.log(friends[0])
+console.log(friends[1])
+console.log(friends[2])*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

9a8b88dd6e1b978a21f82f447571258e45b66d93
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-03-05 03:38:53 +0500

    Q11-15

--- 45 Assignments/main.js
+++ 45 Assignments/main.js
@@ -1,27 +1,52 @@
 "use strict";
-//Assignment 6
-//Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
-/*let personName:string = "\t\n  Haroon Shahid  \n\t"
-console.log("Original:", personName)
-console.log("Stripped:",personName.trim())*/
-//Assignment 7
-//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
-/*console.log(5+3)
-console.log(10-2)
-console.log(2*4)
-console.log(16/2)*/
-//Assignment 8
-//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
-/*let favoriteNumber:number = 7
-console.log(`My favorite number is ${favoriteNumber}`)*/
-//Assignment 9
-//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
-//Author : Bilqees Shahid
-//date : March 1st 2024
-//printing my favourite number in Assignment 8
-//Assignment 10
-// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
-/*let friends =["Sara","Yusra","Fatima"]
-console.log(friends[0])
-console.log(friends[1])
-console.log(friends[2])*/
+//Assignment 11
+//Greetings: Start with the array you used in Exercise 10, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+/*let Names:string[]= ["Sara","Ali","Faizan","Haroon"]
+let message: string= "Do you like to play cricket?"
+console.log(Names[0] +" "+message)
+console.log(Names[1] +" "+message)
+console.log(Names[2] +" "+message)
+console.log(Names[3] +" "+message)*/
+//Assignment 12
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+/*let transportation:string[]=["Honda civic","toyota corolla","Cultus"]
+transportation.map((items) =>
+ console.log(`I would like to own ${items}`))*/
+//Assignment 13
+//Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+/*let Guest:string[]=["Sara","Fatima","Yusra"]
+Guest.map((items) =>
+console.log(`Dear ${items}! you are invited for the dinner today.`)
+)*/
+//Assignment 14
+//Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+/*let Guest:string[]=["Sara","Fatima","Faizan","Haroon"]
+let cannotAttend:string = "Sara"
+console.log(cannotAttend +" "+ "can not attend dinner")
+let newGuest:string= "Aisha"
+Guest[Guest.indexOf(cannotAttend)]= newGuest
+console.log(Guest)
+Guest.map((items)=> console.log(`Dear ${items} "you are invited for dinner today`))*/
+//Assignment 15
+//More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+//• Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
+//• Add one new guest to the beginning of your array.
+//• Add one new guest to the middle of your array. • Use append() to add one new guest to the end of your list. • Print a new set of invitation messages, one for each person in your list.let Guest:string[]=["Aisha","Fatima","Sara","Faizan"]
+//Part1
+/*let Guest:string[]=["Aisha","Fatima","Sara","Faizan"]
+//Guest.map((items)=> console.log(`Dear ${items},I found the biggest dinner table so i am inviting the more people.`))
+//Part2
+let Guestbeg:string = "Haroon"
+Guest.unshift(Guestbeg);
+//console.log(Guest)
+//Part3
+let middleguest:string ="Abdullah"
+let middleindex=Guest.length/2
+Guest.splice(middleindex,0,middleguest)
+//console.log(Guest)
+//Part4
+let endguest:string= "Hassan"
+Guest.push(endguest)
+//console.log(Guest)
+//Part5
+Guest.map((items)=> console.log(`Dear ${items}! you are invited on the biggest dinner table today.`))*/

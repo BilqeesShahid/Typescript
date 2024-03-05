bab7a12297ec21bc6cfdad9619b40cd717aa684e
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-03-06 03:16:07 +0500

    Q21-30

---
+++ Assignments21-30/main.js
@@ -0,0 +1,3 @@
+"use strict";
+//Assignment 21
+//Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:

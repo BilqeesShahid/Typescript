d56af8cde0d29e6676f3ceaeb57cbcf48c9d7d4d
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-03-21 01:38:46 +0500

    calculator

---
+++ calculator/main.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+//Asking questions from users through Inquirer
+let answers = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    { message: "Select one Operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+//conditions if else
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "Subtraction") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("Invalid operator");
+}

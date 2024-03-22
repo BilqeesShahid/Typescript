47a3c76442504427efdd699b7374dbc9d0502ce0
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-03-23 03:51:00 +0500

    NumGuessGame

---
+++ Num_Guess_Game/index.js
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+const systemGeneratedNo = Math.floor(Math.random() * 10);
+//console.log(systemGeneratedNo)
+const answer = await inquirer.prompt([{
+        type: "number",
+        name: "userGuess",
+        message: "Enter your guess b/w 1 to 10"
+    }]);
+const { userGuess } = answer;
+console.log(`your guess,${userGuess},systemNumber,${systemGeneratedNo}`);
+if (userGuess == systemGeneratedNo) {
+    console.log("yes your answer is correct\n YOU WIN");
+}
+else {
+    console.log("Better luck next time");
+}

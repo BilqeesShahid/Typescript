65da8ff8c5876db427639ce8d36476bbb436956e
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-03-29 01:31:23 +0500

    funtions123

---
+++ more functions/main.js
@@ -0,0 +1,17 @@
+"use strict";
+// functions
+//checking #1 program
+function checking(a, b) {
+    console.log(a + b + 1);
+}
+checking(2, 5);
+checking(7, 3);
+//#2 program
+function logSomeNames(fruitName, carName) { console.log(fruitName, carName); }
+logSomeNames("mango", "corolla");
+// #3 program
+function halfFry(salt, butter) {
+    let cooked = salt + butter + 1;
+    return cooked;
+}
+console.log(halfFry(4, 2));
--- more functions/main.ts
+++ more functions/main.ts
@@ -1,1 +1,21 @@
+// functions
 
+//checking #1 program
+function checking(a:number,b:number){
+    console.log(a + b + 1)
+
+}
+checking(2,5);
+checking(7,3)
+
+//#2 program
+function logSomeNames(fruitName:string ,carName:string)
+{console.log(fruitName ,carName)}
+
+logSomeNames("mango","corolla")
+// #3 program
+function halfFry(salt:any,butter:any){
+    let cooked=salt + butter + 1
+return cooked
+}
+console.log(halfFry(4,2))

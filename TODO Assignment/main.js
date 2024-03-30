ae8a145200f3b19fb716fac6f1f31a91c2f96b16
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-03-31 03:11:05 +0500

    TODO Assignment

---
+++ TODO Assignment/main.js
@@ -0,0 +1,19 @@
+import inquirer from "inquirer";
+let toDos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([{
+            name: "todo",
+            type: "input",
+            message: "What do you want in your arrays?"
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more?",
+            default: "false"
+        }]);
+    toDos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(toDos);
+}
--- TODO Assignment/main.ts
+++ TODO Assignment/main.ts
@@ -1,1 +1,24 @@
+#! /usr/bin/env node 
 
+import inquirer from "inquirer";
+let toDos = [];
+let condition= true;
+while(condition){
+
+let addTask=await  inquirer.prompt([{
+    name:"todo",
+    type:"input",
+    message:"What do you want in your arrays?"
+},
+{
+    name:"addMore",
+    type:"confirm",
+    message:"Do you want to add more?",
+    default:"false"
+
+}]
+);
+toDos.push(addTask.todo)
+condition=addTask.addMore
+console.log(toDos)
+}

edee7eced5ceb4a6ac994b8d53f37fd97769e07a
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-04-17 03:15:34 +0500

    my-word_count-app

--- my-word_count-app/main.ts
+++ my-word_count-app/main.ts
@@ -1,1 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answers:{sentence:string
+}=await inquirer.prompt([{
 
+    name :"sentence",
+    type:"input",
+    message:"Enter your sentence to count the word"
+}])
+//console.log(answers)
+const words= answers.sentence.trim().split(" ")
+console.log(words)
+console.log(`your sentence word count is ${words.length}`);

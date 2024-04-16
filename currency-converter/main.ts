b1971938033bcea4a3468e2342279145bbdcec0a
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-04-17 02:50:25 +0500

    rapid-currency-converter

--- currency-converter/main.ts
+++ currency-converter/main.ts
@@ -1,1 +1,43 @@
+#! /usr/bin/env node
 
+import inquirer from "inquirer";
+import ListPrompt from "inquirer/lib/prompts/list.js";
+const currency:any ={
+    USD:1, //base currency
+    EUR:0.91,
+    GBP:0.76,
+    INR:74.57,
+    PKR:280
+}
+
+
+
+let user_answer = await inquirer.prompt([{
+    name:"from",
+    message:"Enter from currency",
+    type:"list",
+    choices:["USD","EUR","GBP","INR","PKR"]
+}
+,
+{ name:"to",
+message:"Enter to currency",
+type:"list",
+choices:["USD","EUR","GBP","INR","PKR"]
+},
+{
+    name:"amount",
+    message:"Enter your amount",
+    type:"number",
+    
+}])
+let fromAmount=currency[user_answer.from]
+let toAmount= currency[user_answer.to]
+let amount = user_answer.amount
+let baseAmount = amount/fromAmount //USD base currency//4
+let convertedAmount = baseAmount * toAmount
+console.log(convertedAmount)
+
+
+console.log(user_answer.from)
+console.log(user_answer.to)
+console.log(user_answer.amount)
